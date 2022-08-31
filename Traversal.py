0859e7cd9fc6e1bc570cef5adc4d12b58fb597e5
Rohit Mehta <rmehta.bitspilani@gmail.com>
2022-08-31 19:23:38 +0530

    Update Traversal.py

--- Traversal.py
+++ Traversal.py
@@ -6,3 +6,16 @@ for i in range(1,len(l)):
   if l[i] > v:
     v = l[i]
 print(v)
+
+#Code snippet for solving lot of codechef problems:
+i =int(input())
+for j in range(i*2):
+    if j%2 == 0:
+        f,s = map(int,input().split())
+        A=1;
+    else:
+        nf,ns = map(int,input().split())
+        if not (nf < f) and not (ns < s):
+            print('POSSIBLE')
+        else:
+            print ('IMPOSSIBLE')
