73774d0f6776958fdae6ad7df531a247d4d6a9ee
Rohit Mehta <rmehta.bitspilani@gmail.com>
2022-12-19 19:04:14 +0530

    Update Algorithms_metrics.py

--- Algorithms_metrics.py
+++ Algorithms_metrics.py
@@ -1,2 +1,66 @@
 # List of all the machine learning algorithms and the code to initiate the models along with the metrics for model performance 
 # depending upon data imbalance, outliers, number of columns, missing values, presence of noise and the structure of data statistically.
+
+# There are many machine learning algorithms that can be used for a wide variety of tasks and data types.
+# Here is a list of some common algorithms and the code to initiate them in Python using scikit-learn:
+
+# 1) Linear regression: This is a simple algorithm used for predicting a continuous target variable based on one or more independent variables.
+
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+
+# 2) Logistic regression: This is a classification algorithm used for predicting a binary target variable based on one or more independent variables.
+
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+
+# 3) K-nearest neighbors (KNN): This is a classification or regression algorithm that makes predictions 
+# based on the values of the nearest neighbors in the training data.
+
+from sklearn.neighbors import KNeighborsClassifier
+model = KNeighborsClassifier(n_neighbors=5)
+
+# 4) Decision tree: This is a tree-based algorithm that makes predictions based on a series of decisions based on the features of the data.
+
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier()
+
+# 5) Random forest: This is an ensemble algorithm that builds multiple decision trees and combines their predictions to make a final prediction.
+
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier()
+
+# 6) Support vector machine (SVM): This is a classification or regression algorithm that finds the hyperplane that maximally 
+# separates classes or predicts continuous values.
+
+from sklearn.svm import SVC
+model = SVC()
+
+# 7) Naive Bayes: This is a classification algorithm based on the assumption of independence between features.
+
+from sklearn.naive_bayes import GaussianNB
+model = GaussianNB()
+
+# 8) Neural network: This is a machine learning algorithm that consists of multiple layers of interconnected nodes and is 
+# inspired by the structure and function of the human brain.
+
+from sklearn.neural_network import MLPClassifier
+model = MLPClassifier()
+
+# In terms of model performance metrics, there are many factors to consider, including data imbalance, outliers, number of columns, missing values, presence of noise, and the structure of the data. Some common metrics for evaluating the performance of a machine learning model include:
+
+# 1) Accuracy: This is the percentage of correct predictions made by the model.
+# 2) Precision: This is the number of true positive predictions made by the model divided by the total number of positive predictions made.
+# 3) Recall: This is the number of true positive predictions made by the model divided by the total number of actual positive cases in the data.
+# 4) F1 score: This is the harmonic mean of precision and recall.
+# 5) ROC-AUC curve: This is a curve that plots the true positive rate against the false positive rate at different classification thresholds.
+# 6) Confusion matrix: This is a matrix that displays the number of true positive, true negative, false positive, and false negative predictions made by the model.
+
+# It is important to choose the appropriate metric depending on the characteristics of the data and the goals of the model.
+
+# For example, if the data is imbalanced, it may be more important to focus on precision or recall rather than overall accuracy. If the data has many outliers
+# or is highly skewed, it may be necessary to use metrics that are less sensitive to these issues, such as the ROC-AUC curve or the F1 score.
+# In terms of dealing with missing values, one common approach is to impute the missing values using the mean, median, or mode of the available values. Another option is to use a machine learning algorithm specifically designed to handle missing values, such as multiple imputation.
+# If the data contains a lot of noise or irrelevant features, it may be helpful to use feature selection techniques to identify and remove these features before training the model. Some common techniques include backward elimination, forward selection, and recursive feature elimination.
+# It is also important to consider the structure of the data when selecting a machine learning algorithm. For example, if the data is linearly separable, a linear model such as linear regression or logistic regression may be appropriate. If the data is more complex and nonlinear, 
+# a nonlinear model such as a decision tree or a neural network may be more appropriate.
