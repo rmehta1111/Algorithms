87636daa3c69708f36019916b2ed481484afd0e9
Rohit Mehta <rmehta.bitspilani@gmail.com>
2022-12-19 18:53:01 +0530

    Create Algorithms_metrics.py

---
+++ Algorithms_metrics.py
@@ -0,0 +1,2 @@
+# List of all the machine learning algorithms and the code to initiate the models along with the metrics for model performance 
+# depending upon data imbalance, outliers, number of columns, missing values, presence of noise and the structure of data statistically.
